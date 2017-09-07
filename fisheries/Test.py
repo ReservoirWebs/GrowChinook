35b448ae0aabab0a3faba6c4df36ca16977c61a2
johnbren85 <johnsonbrent38@gmail.com>
2017-09-07 12:07:53 -0700

    Add files via upload

--- fisheries/Test.py
+++ fisheries/Test.py
@@ -2,12 +2,12 @@
 
 import os
 import glob
-#import html
+import cgi
 import PrintPages as pt
 
-#address = html.escape(os.environ["REMOTE_ADDR"])
-#script = "Main Model Form"
-#pt.write_log_entry(script, address)
+address = cgi.escape(os.environ["REMOTE_ADDR"])
+script = "Main Model Form"
+pt.write_log_entry(script, address)
 pt.print_header('GrowChinook')
 pt.print_full_form(None, None, 'in')
 extension = 'csv'
